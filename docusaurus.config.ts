896644366fad513f51a0f73f7c6c9007721874e2
Yukihisa Fujita <yukihisa.fujita@toyota-tokyo.tech>
2025-09-04 02:13:25 +0000

    initial commit of base file structures

---
+++ docusaurus.config.ts
@@ -0,0 +1,108 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'MaaS Blender',
+  // tagline: '',
+  // favicon: 'img/maasblender.icns',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://maasblender.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'maasblender', // Usually your GitHub org/user name.
+  projectName: 'maasblender-docs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+    localeConfigs: {
+      en: {
+        label: 'English',
+      },
+      ja: {
+        label: '日本語',
+      },
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: 'current',
+              path: '',
+            },
+          },
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.ts',
+        },
+        blog: false, // Optional: disable the blog plugin
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'MaaS Blender Docs',
+      logo: {
+        alt: 'MaaS Blender Logo',
+        src: 'img/maasblender.jpeg',
+      },
+      items: [
+        {
+          type: 'docsVersionDropdown',
+          versions: ['current'],
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/maasblender/maasblender',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} TOYOTA MOTOR CORPORATION and MaaS Blender Contributors.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
